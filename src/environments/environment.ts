732b783eb1ea24e8dc66be4381f79588c47e528c
Brian Freijomil <brianfreijomil@hotmail.com>
2025-02-22 12:42:35 -0300

    fix() backend endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api', //API URL
+    apiUrl: 'http://192.168.0.99:8080/api', //API URL
     appName: 'desafio-backend',
   };
   
